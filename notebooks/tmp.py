8391d614be9c3981210fdefb0454258c4aa9e429
Mystic98 <iantonov.zhenya@gmail.com>
2026-02-16 18:22:33 +0300

    starting the hw1

---
+++ notebooks/tmp.py
@@ -0,0 +1,5 @@
+from hw1.data_preprocessing import print_tensor
+import torch
+
+x = torch.tensor([[1, 2], [3, 4]])
+print_tensor(x)
---
+++ src/hw1/data_preprocessing.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import torch
+from numpy.typing import ArrayLike
+
+
+def print_tensor(x: ArrayLike):
+    return print(x)
